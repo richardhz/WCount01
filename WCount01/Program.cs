f33fcf98bfae181cbb332b5cef9a1ec96ad0a1af
Richard Hernandez <r.a.hernandez@outlook.com>
2019-11-17 17:01:03 +0000

    Implement word count using linq

--- WCount01/Program.cs
+++ WCount01/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace WCount01
 {
@@ -6,7 +9,38 @@ namespace WCount01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Please enter file path");
+                return;
+            }
+            else
+            {
+                string text = null;
+                string textFileName = args[0];
+                try
+                {
+                    text = File.ReadAllText(textFileName);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return;
+                }
+
+                Regex reg_exp = new Regex("[^a-zA-Z0-9]");
+
+                var wordString = reg_exp.Replace(text, " ").ToLower();
+                var words = wordString.Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+
+                var groups = words.GroupBy(w => w).OrderByDescending(g => g.Count()).Take(20);
+
+                foreach (var item in groups)
+                {
+                    Console.WriteLine($"{item.Count()} {item.Key}");
+                }
+            }
         }
     }
 }
